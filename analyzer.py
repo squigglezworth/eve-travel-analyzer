07c746c42d845bed38f86e7c337632ca6098e7d2
squigz <squigglezworth@gmail.com>
2019-11-25 16:13:57 -0500

    Format as flat list
    always overthinking things...

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,4 @@
-import sys, re, json
+import sys, re
 
 # Change to your character's name
 listeners = ['Sen Isu']
@@ -8,7 +8,7 @@ listeners = ['Sen Isu']
 # And first channel change, since that is from the session start
 START_LINE = 13
 CHANNELS = ['local']
-jumps = {}
+jumps = []
 for file in sys.argv[1:]:
 	with open(file, encoding="utf-16") as f:
 		lines = f.readlines()
@@ -18,14 +18,17 @@ for file in sys.argv[1:]:
 
 		if (channel in CHANNELS and listener in listeners):
 			session = lines[9].strip().split()[-2:]
-			if session[0] in jumps:
-				jumps[session[0]].update({session[1]: []})
-			else:
-				jumps[session[0]] = {session[1]: []}
+
+			# if session[0] in jumps:
+			# 	jumps[session[0]].update({session[1]: []})
+			# else:
+			# 	jumps[session[0]] = {session[1]: []}
 
 			for line in lines[START_LINE:]:
 				if re.search('Channel changed to Local', line):
 					results = re.search("\[ .* (.*) \].*Channel\ changed\ to\ Local\ :\ (.*)", line)
-					jumps[session[0]][session[1]].append({results.group(1): results.group(2)})
+					jumps.append('{}\t{}\t{}'.format(session[0], results.group(1), results.group(2)))
 
-print(json.dumps(jumps, indent=4, sort_keys=True))
+					# jumps[session[0]][session[1]].append({results.group(1): results.group(2)})
+
+print(jumps)
